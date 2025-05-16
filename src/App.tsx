0e24b92e7142557a577a3d09bfc98c7cc8813022
Alex S <habahabahaba@outlook.com>
2025-05-16 20:29:56 +0400

    Add document.fonts.ready to the loader.

--- src/App.tsx
+++ src/App.tsx
@@ -36,9 +36,9 @@ function App() {
           );
           await loader();
           // Wait a tick to allow the browser to register @font-face from the injected <style>
-          await new Promise((resolve) => setTimeout(resolve, 100));
+          // await new Promise((resolve) => setTimeout(resolve, 100));
           await document.fonts.load(`600 1em "${font}"`);
-
+          await document.fonts.ready;
           if (document.fonts.check(`600 1em "${font}"`)) {
             markLoadedFont(font as RemoteFontNames);
             numberOfPendingRef.current--;
